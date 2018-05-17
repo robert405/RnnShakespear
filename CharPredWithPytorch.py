399f6fc39dcc9551ab48aedb7630e653875ce533
robert405 <martinrobert405@gmail.com>
2018-05-17 18:44:18 -0400

    Use mse to check problem is with crossEntropy

--- CharPredWithPytorch.py
+++ CharPredWithPytorch.py
@@ -8,14 +8,12 @@ import time
 def predToOneHot(pred):
 
     #idx = np.argmax(pred[0])
-    sortIdx = np.argsort(pred[0])
+    sortIdx = np.argsort(pred)
     reverseSortIdx = sortIdx[::-1]
     randomInt = randint(0,2)
     idx = reverseSortIdx[randomInt]
-    oneHot = np.zeros_like(pred)
-    oneHot[0,idx] = 1
 
-    return oneHot
+    return idx
 
 # ==============================================================================
 # fetch data
@@ -39,7 +37,7 @@ vectSize = len(allChar) # = 77
 for char in allChar:
 
     vect = np.zeros(vectSize)
-    vect[index] = 1
+    vect[index] = 3
     charToVect[char] = vect
     index += 1
 
@@ -87,7 +85,7 @@ class SimpleRNN(nn.Module):
         return outputs, (hx,cx)
 
 model = SimpleRNN(nbNeuron).cuda()
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 # ==============================================================================
@@ -95,40 +93,50 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 # how many char in the dataset and how separate to make batche size of 25 x 25
 #4366250 / 25 -> 174650, 174650 / 25 -> 6986
 
-loopStep = 25 + batch_size
-nbEpoch = 3
-nbChar = len(strList) - loopStep - 1
+seqLentgh = 25 # real length is 24, 25 for last label
+nbEpoch = 25
 lossList = []
-stepCounter = 1
-totalStep = int((nbChar / loopStep) * nbEpoch)
+decal = 5
 
 start = time.time()
 
 for k in range(nbEpoch):
 
+    decal = decal + 1
+    loopStep = seqLentgh + (batch_size * decal)
+    nbChar = len(strList) - loopStep - 1
+    stepCounter = 1
+    epochStep = int(nbChar / loopStep)
+
     for i in range(0, nbChar, loopStep):
 
-        matrix = []
+        data = []
 
-        for j in range(25):
+        for j in range(batch_size):
 
-            idx = i+j
-            matrix += [vectList[idx:idx+batch_size]]
+            idx = i+(j*decal)
+            data += [vectList[idx:idx+seqLentgh]]
 
-        matrix = np.array(matrix)
+        data = np.array(data)
 
-        xData = matrix[0:24]
+        temp = []
+        for idx in range(seqLentgh):
+            temp += [data[:,idx]]
+
+        data = np.array(temp)
+
+        xData = data[0:24]
         xData = torch.FloatTensor(xData).cuda()
 
-        yData = matrix[1:25]
-        yData = np.argmax(yData, axis=2)
-        yData = torch.LongTensor(yData)
-        yData = yData.view(24*batch_size).cuda()
+        yData = data[1:25]
+        #yData = np.argmax(yData, axis=2)
+        yData = torch.FloatTensor(yData)
+        #yData = yData.view(24*batch_size).cuda()
 
         outputs, hidden = model(xData, True)
 
         optimizer.zero_grad()
-        loss = criterion(outputs.view(-1, 77).cuda(), yData)
+        loss = criterion(outputs, yData)
         loss.backward()
         optimizer.step()
 
@@ -136,8 +144,17 @@ for k in range(nbEpoch):
         lossList += [currentLoss]
 
         if ((stepCounter - 1) % 500 == 0):
-            print("Step : " + str(stepCounter) + " / " + str(totalStep) + ", Current Loss : " + str(currentLoss))
+            print("Epoch : " + str(k) + " / " + str(nbEpoch) + ", Step : " + str(stepCounter) + " / " + str(epochStep) + ", Current Loss : " + str(currentLoss))
         stepCounter += 1
+
+    end = time.time()
+    timeTillNow = end - start
+    predictedRemainingTime = (timeTillNow / (k + 1)) * (nbEpoch - (k + 1))
+    print("--------------------------------------------------------------------")
+    print("Finished epoch : " + str(k))
+    print("Time to run since started (sec) : " + str(timeTillNow))
+    print("Predicted remaining time (sec) : " + str(predictedRemainingTime))
+    print("--------------------------------------------------------------------")
 
 
 end = time.time()
@@ -168,8 +185,7 @@ for char in charsToTest:
 
     for vect in predictions:
 
-        bestOptions = vect[0].argsort()[::-1]
-        index = int(bestOptions[np.random.randint(0,3)])
+        index = predToOneHot(vect[0])
         strPred += allChar[index]
 
     strPred += "\n\n\n"
